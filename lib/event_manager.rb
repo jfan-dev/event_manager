9efcb1907804a9de65bfa06889761db6b3d61c10
jfan-dev <122750665+jfan-dev@users.noreply.github.com>
2024-01-09 11:37:09 -0300

    Creat a method to fetch  legislators by zipcode

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,14 +1,32 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
 
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
-puts ''
-puts 'Event Manager Initialized!'
-puts ''
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+    legislator_names = legislators.map(&:name)
+    legislators_string = legislator_names.join(", ")
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+puts ' '
+puts 'EventManager initialized.!!!'
+puts ' '
 
 contents = CSV.open(
   'event_attendees.csv',
@@ -21,5 +39,7 @@ contents.each do |row|
 
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
